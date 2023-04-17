96835883d1065a62e7b6ac599ec814b670711c90
zhangsheng <yanglee2421@gmail.com>
2023-04-17 17:19:49 +0800

    sort out code

--- qiankun-app/src/main.tsx
+++ qiankun-app/src/main.tsx
@@ -2,9 +2,51 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "@/assets/css/reset.css";
+import {
+  registerMicroApps,
+  start,
+  setDefaultMountApp,
+  RegistrableApp,
+  ObjectType,
+} from "qiankun";
+
+registerMicroApps(microApps());
+
+setDefaultMountApp("/vite-vue");
+
+// 启动 qiankun
+start({
+  sandbox: {
+    experimentalStyleIsolation: true,
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+function microApps(): RegistrableApp<ObjectType>[] {
+  return [
+    {
+      name: "reactApp",
+      entry: "//localhost:3000",
+      // entry: "//localhost:5174",
+      container: "#container",
+      activeRule: "/vite-react",
+    },
+    {
+      name: "vueApp",
+      entry: "//localhost:3001",
+      container: "#container",
+      activeRule: "/vite-vue",
+    },
+    {
+      name: "mvc",
+      entry: "//localhost:5500",
+      container: "#container",
+      activeRule: "/mvc",
+    },
+  ];
+}
